8e98e835a0eaae8c7c78089fb1f9607feb388a71
Greycin Kim <kimgreycin@gmail.com>
2025-04-03 00:11:07 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Player from "./Components/Player.jsx";
+
+function App() {
+  return (
+      <main>
+        <div id="game-container">
+          <ol id="players">
+              <Player initialName={"Player 1"} symbol={"X"}></Player>
+              <Player initialName={"Player 2"} symbol={"O"}></Player>
+          </ol>
+          GAMEBOARDds
+        </div>
+
+        LOG
+      </main>
+  );
+}
+
+export default App
